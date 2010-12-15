c99ba6d16554f7326f250f527636c2495cd6c010
Clint Newsom <hcnewsom@dyn-209-2-234-251.dyn.columbia.edu>
2010-12-15 16:47:58 -0500

    a ton of changes

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,33 @@
+var sort;
+
 function getFormData(type) {
-	//We can use one Request object many times.
-	var req = new Request.HTML({url:'http://10.0.1.9/~hcnewsom/forms-interface/js/'+type+'.html', 
+	var req = new Request.HTML({url:'http://dyn-209-2-234-251.dyn.columbia.edu/~hcnewsom/CU-eForms/js/'+type+'.html', 
 		append: $('form-canvas'),
 		onFailure: function() {
 			$('form-canvas').set('text', 'The request failed.');
-		}
+		}, 
+	    onSuccess: onFormData
 	});
 	req.send();	
+}
+
+function onFormData() {
+	sort.addItems($$('#form-canvas li').getLast());
+	$$('.label').addEvent('click', function(e){	
+	    $(this).addClass('hidden');
+		var openClass = $(this).getNext('.label-input');
+		openClass.setStyle('opacity', 0);
+		openClass.removeClass('hidden');
+		openClass.fade('in');
+		openClass.setStyle('background-color', '#fff6db');
+	});	
+	keyEnter($$('.label-input'));
+}
+
+function keyEnter(element){	
+	element.addEvent('keydown:keys(enter)', function(){
+		alert("you pressed enter");
+	});
 }
 
 window.addEvent('domready', function() {
@@ -20,10 +41,31 @@ window.addEvent('domready', function() {
 		'li.dropdown': ''
 	};
 	
-	$H(selectorToFormData).each(function(type, selector) {
-		$$(selector).addEvent("click", function(e) {
-			getFormData(type);
-		});
+    sort = new Sortables('form-canvas', {
+	   handle: '.form-elem',
+	   constrain: true,
+	   clone: true
 	});
+	
+	$H(selectorToFormData).each(function(type, selector) {
+		$$(selector).addEvent("click", function(e) { 
+		   getFormData(type);
 
+	  });
+	});
+		
+	$$('.tab-open').addEvent("click", function(e){
+		$$('.tab-open').each(function(element, selector){
+			$$(element).removeClass('pressed');
+		});
+		$(this).addClass('pressed');
+		
+		$$('.tab').each(function(element, selector) {
+			$$(element).fade('out');
+			console.log(element);
+		});
+		var openIdentifier = '#' + $$(this).get('open')[0];
+		$$(openIdentifier).fade('in');
+	});
+				
 });
---
+++ js/pseudos-keys.js
@@ -0,0 +1,291 @@
+// MooTools: the javascript framework.
+// Load this file's selection again by visiting: http://mootools.net/more/2b840f1cba3d9a0f15ccc0d60c2ed3ed 
+// Or build this file again with packager using: packager build More/Element.Event.Pseudos.Keys
+/*
+---
+
+script: More.js
+
+name: More
+
+description: MooTools More
+
+license: MIT-style license
+
+authors:
+  - Guillermo Rauch
+  - Thomas Aylott
+  - Scott Kyle
+  - Arian Stolwijk
+  - Tim Wienk
+  - Christoph Pojer
+  - Aaron Newton
+
+requires:
+  - Core/MooTools
+
+provides: [MooTools.More]
+
+...
+*/
+
+MooTools.More = {
+	'version': '1.3.0.1',
+	'build': '6dce99bed2792dffcbbbb4ddc15a1fb9a41994b5'
+};
+
+
+/*
+---
+
+name: Events.Pseudos
+
+description: Adds the functionallity to add pseudo events
+
+license: MIT-style license
+
+authors:
+  - Arian Stolwijk
+
+requires: [Core/Class.Extras, Core/Slick.Parser, More/MooTools.More]
+
+provides: [Events.Pseudos]
+
+...
+*/
+
+Events.Pseudos = function(pseudos, addEvent, removeEvent){
+
+	var storeKey = 'monitorEvents:';
+
+	var storageOf = function(object){
+
+		return {
+			store: object.store ? function(key, value){
+				object.store(storeKey + key, value);
+			} : function(key, value){
+				(object.$monitorEvents || (object.$monitorEvents = {}))[key] = value;
+			},
+			retrieve: object.retrieve ? function(key, dflt){
+				return object.retrieve(storeKey + key, dflt);
+			} : function(key, dflt){
+				if (!object.$monitorEvents) return dflt;
+				return object.$monitorEvents[key] || dflt;
+			}
+		};
+	};
+
+
+	var splitType = function(type){
+		if (type.indexOf(':') == -1) return null;
+
+		var parsed = Slick.parse(type).expressions[0][0],
+			parsedPseudos = parsed.pseudos;
+
+		return (pseudos && pseudos[parsedPseudos[0].key]) ? {
+			event: parsed.tag,
+			value: parsedPseudos[0].value,
+			pseudo: parsedPseudos[0].key,
+			original: type
+		} : null;
+	};
+
+
+	return {
+
+		addEvent: function(type, fn, internal){
+			var split = splitType(type);
+			if (!split) return addEvent.call(this, type, fn, internal);
+
+			var storage = storageOf(this),
+				events = storage.retrieve(type, []),
+				pseudoArgs = Array.from(pseudos[split.pseudo]),
+				proxy = pseudoArgs[1];
+
+			var self = this;
+			var monitor = function(){
+				pseudoArgs[0].call(self, split, fn, arguments, proxy);
+			};
+
+			events.include({event: fn, monitor: monitor});
+			storage.store(type, events);
+
+			var eventType = split.event;
+			if (proxy && proxy[eventType]) eventType = proxy[eventType].base;
+
+			addEvent.call(this, type, fn, internal);
+			return addEvent.call(this, eventType, monitor, internal);
+		},
+
+		removeEvent: function(type, fn){
+			var split = splitType(type);
+			if (!split) return removeEvent.call(this, type, fn);
+
+			var storage = storageOf(this),
+				events = storage.retrieve(type),
+				pseudoArgs = Array.from(pseudos[split.pseudo]),
+				proxy = pseudoArgs[1];
+
+			if (!events) return this;
+
+			var eventType = split.event;
+			if (proxy && proxy[eventType]) eventType = proxy[eventType].base;
+
+			removeEvent.call(this, type, fn);
+			events.each(function(monitor, i){
+				if (!fn || monitor.event == fn) removeEvent.call(this, eventType, monitor.monitor);
+				delete events[i];
+			}, this);
+
+			storage.store(type, events);
+			return this;
+		}
+
+	};
+
+};
+
+(function(){
+
+var pseudos = {
+
+	once: function(split, fn, args){
+		fn.apply(this, args);
+		this.removeEvent(split.original, fn);
+	}
+
+};
+
+Events.definePseudo = function(key, fn){
+	pseudos[key] = fn;
+};
+
+var proto = Events.prototype;
+Events.implement(Events.Pseudos(pseudos, proto.addEvent, proto.removeEvent));
+
+})();
+
+
+/*
+---
+
+name: Element.Event.Pseudos
+
+description: Adds the functionality to add pseudo events for Elements
+
+license: MIT-style license
+
+authors:
+  - Arian Stolwijk
+
+requires: [Core/Element.Event, Events.Pseudos]
+
+provides: [Element.Event.Pseudos]
+
+...
+*/
+
+(function(){
+
+var pseudos = {
+
+	once: function(split, fn, args){
+		fn.apply(this, args);
+		this.removeEvent(split.original, fn);
+	}
+
+};
+
+Event.definePseudo = function(key, fn, proxy){
+	pseudos[key] = [fn, proxy];
+};
+
+var proto = Element.prototype;
+[Element, Window, Document].invoke('implement', Events.Pseudos(pseudos, proto.addEvent, proto.removeEvent));
+
+})();
+
+
+/*
+---
+
+name: Element.Event.Pseudos.Keys
+
+description: Adds functionallity fire events if certain keycombinations are pressed
+
+license: MIT-style license
+
+authors:
+  - Arian Stolwijk
+
+requires: [Element.Event.Pseudos]
+
+provides: [Element.Event.Pseudos.Keys]
+
+...
+*/
+
+(function(){
+
+var keysStoreKey = '$moo:keys-pressed',
+	keysKeyupStoreKey = '$moo:keys-keyup';
+
+
+Event.definePseudo('keys', function(split, fn, args){
+
+	var event = args[0],
+		keys = [],
+		pressed = this.retrieve(keysStoreKey, []);
+
+	keys.append(split.value.replace('++', function(){
+		keys.push('+'); // shift++ and shift+++a
+		return '';
+	}).split('+'));
+
+	pressed.include(event.key);
+
+	if (keys.every(function(key){
+		return pressed.contains(key);
+	})) fn.apply(this, args);
+
+	this.store(keysStoreKey, pressed);
+
+	if (!this.retrieve(keysKeyupStoreKey)){
+		var keyup = function(event){
+			(function(){
+				pressed = this.retrieve(keysStoreKey, []).erase(event.key);
+				this.store(keysStoreKey, pressed);
+			}).delay(0, this); // Fix for IE
+		};
+		this.store(keysKeyupStoreKey, keyup).addEvent('keyup', keyup);
+	}
+
+});
+
+Object.append(Event.Keys, {
+	'shift': 16,
+	'control': 17,
+	'alt': 18,
+	'capslock': 20,
+	'pageup': 33,
+	'pagedown': 34,
+	'end': 35,
+	'home': 36,
+	'numlock': 144,
+	'scrolllock': 145,
+	';': 186,
+	'=': 187,
+	',': 188,
+	'-': Browser.firefox ? 109 : 189,
+	'.': 190,
+	'/': 191,
+	'`': 192,
+	'[': 219,
+	'\\': 220,
+	']': 221,
+	"'": 222,
+	'+': 107
+});
+
+})();
+
