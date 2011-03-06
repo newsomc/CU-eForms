c30a3bddcc596764a853a118674a2b85a476a442
Clint Newsom <hcnewsom@Clint-Newsoms-MacBook-Pro.local>
2011-03-06 10:40:32 -0500

    adding interaction to form elements. unfinished.

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,7 @@ function getFormData(type) {
 
 function onFormData() {
 	sort.addItems($$('#form-canvas li').getLast());
+	
 	$$('.label').addEvent('click', function(e){	
 	    $(this).addClass('hidden');
 		var openClass = $(this).getNext('.label-input');
@@ -20,10 +21,26 @@ function onFormData() {
 		openClass.setStyle('opacity', 0);
 		openClass.removeClass('hidden');
 		openClass.fade('in');
+		console.log(openClass);
 		openClass.setStyle('background-color', '#fff6db');			
 	});	
-	$$('.label-input').addEvent('keydown:keys(enter)', function(){
-		$(this).setStyle('background-color', 'red');
+	
+	$$('.label-input').addEvent('keydown:keys(enter)', function(){		
+		var label = $(this).getPrevious('.label');
+		var inputValue = this.value;
+		$(this).addClass('hidden');
+		label.removeClass('hidden');
+        test = label.set('text', inputValue);
+	});
+	
+	//List element interaction. TODO: needs to be set to toggle.
+	$$('.editFieldButton').addEvent('click', function(){
+	  	var thisElem = $(this).getParent('.form-elem');
+	    var myFx = new Fx.Tween(thisElem);
+		myFx.set('background-color', '#f00');	
+		console.log($$('#form-tools-two'));
+		$$('#form-tools-two').get('open')[0];
+		$$('#form-tools-two').fade('in');
 	});
 }
 
@@ -47,10 +64,10 @@ window.addEvent('domready', function() {
 	$H(selectorToFormData).each(function(type, selector) {
 		$$(selector).addEvent("click", function(e) { 
 		   getFormData(type);
-
 	  });
 	});
-		
+	
+	// form-tools tabs	
 	$$('.tab-open').addEvent("click", function(e){
 		$$('.tab-open').each(function(element, selector){
 			$$(element).removeClass('pressed');
@@ -59,11 +76,28 @@ window.addEvent('domready', function() {
 		
 		$$('.tab').each(function(element, selector) {
 			$$(element).fade('out');
+			$$(element).removeClass('hidden');
 			console.log(element);
 		});
 		var openIdentifier = '#' + $$(this).get('open')[0];
 		console.log(openIdentifier);
 		$$(openIdentifier).fade('in');
 	});
-				
+	
+	// header tabs
+	$$('.header-tab').addEvent("click", function(e){
+		$$('.header-tab').each(function(element, selector){
+			$$(element).removeClass('header-pressed');
+		});
+		$(this).addClass('header-pressed');
+		
+		$$('.header').each(function(element, selector) {
+			$$(element).fade('out');
+			console.log(element);
+		});
+		var openIdentifier = '#' + $$(this).get('open')[0];
+		console.log(openIdentifier);
+		$$(openIdentifier).fade('in');
+	});
+						
 });
